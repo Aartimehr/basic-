709290abb0bf08c09823264aac26d3a26f7a109f
Abecedarian <106958470+Aartimehr@users.noreply.github.com>
2022-10-29 00:55:20 +0530

    Create functions.cpp

---
+++ functions.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+using namespace std;
+ void printnumbers(int n,int m)
+ {
+     cout<<"numbers are"<<endl;
+ }
+ int main()
+ {
+     int n=90;
+     int m=95;
+     printnumbers(n,m);
+     cout<<n<<" "<<m<<" "; 
+     return 0;
+ }
+ 
